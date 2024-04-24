3bcbe1b2487c661ef384ba9b6f231b9c5a485b57
Morgikc <144792250+Morgikc@users.noreply.github.com>
2024-04-24 23:22:13 +0300

    Create task.h

---
+++ task.h
@@ -0,0 +1,51 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <string>
+#include <QVector>
+
+class Time
+{
+public:
+    Time() = default;
+    Time(int,int);
+    ~Time();
+
+private:
+    int hours;
+    int minutes;
+};
+
+class Date
+{
+private:
+    std::string day;
+    std::string month;
+    std::string year;
+public:
+    Date() = default;
+    Date(std::string, std::string, std::string);
+    ~Date();
+};
+
+class Task
+{
+private:
+    std::string description;
+    Time start;
+    Time finish;
+
+public:
+    Task() = default;
+    Task(std::string, Time, Time);
+    ~Task();
+};
+
+
+struct Day_Tasks
+{
+  Date day_Today;
+  QVector <Task> tasks;
+};
+
+#endif // TASK_H
