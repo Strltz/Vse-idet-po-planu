e07496bfe91983359508d74c2f7fcd40fe27f75b
Morgikc <144792250+Morgikc@users.noreply.github.com>
2024-04-25 15:42:32 +0300

    Update task.h
    Для сортировок и вывода

--- task.h
+++ task.h
@@ -3,43 +3,50 @@
 
 #include <string>
 #include <QVector>
+#include <QString>
+#include <QtAlgorithms>
 
 class Time
 {
 public:
     Time() = default;
     Time(std::string,std::string);
+    std::string toStr();
     ~Time();
 
-private:
+
     std::string hours;
     std::string minutes;
 };
 
 class Date
 {
-private:
+public:
     std::string day;
     std::string month;
     std::string year;
-public:
+
     Date() = default;
     Date(std::string, std::string, std::string);
     void setDate(std::string, std::string, std::string);
+    std::string toStr();
     ~Date();
+    friend std::ostream& operator<<(std::ostream&, Date&);
 };
 
 class Task
 {
-private:
+public:
     std::string description;
     Time start;
     Time finish;
 
-public:
     Task() = default;
     Task(std::string, Time, Time);
     ~Task();
+    friend std::ostream& operator<<(std::ostream&, Task&);
+    bool friend Sort(const Task&, const Task&);
+    friend bool operator<(Task, Task);
 };
 
 
@@ -48,5 +55,7 @@ struct Day_Tasks
   Date day_Today;
   QVector <Task> tasks;
 };
+
+bool operator<(Day_Tasks, Day_Tasks);
 
 #endif // TASK_H
