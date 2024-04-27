0ed67fd52ebf9d8690389f65b106da532ac70b0c
Morgikc <144792250+Morgikc@users.noreply.github.com>
2024-04-27 17:38:41 +0300

    Update secondwindow.h

--- secondwindow.h
+++ secondwindow.h
@@ -1,6 +1,10 @@
+
 #ifndef SECONDWINDOW_H
 #define SECONDWINDOW_H
 
+#include "qdynamicbutton.h"
+#include "entertask.h"
+#include "task.h"
 #include <QDialog>
 
 namespace Ui {
@@ -13,10 +17,19 @@ class SecondWindow : public QDialog
 
 public:
     explicit SecondWindow(QWidget *parent = 0);
+    void setVector(const QVector <Day_Tasks>&);
     ~SecondWindow();
+
+private slots:
+    void on_add_clicked();
+    void on_deleteBut_clicked();
+    void slotGetNumber();
 
 private:
     Ui::SecondWindow *ui;
+    EnterTask *newTask;
+    QVector <Day_Tasks> week;
+
 };
 
 #endif // SECONDWINDOW_H
