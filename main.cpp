8c7b2c991dbe135926fd0a3c8929e0c58b80895c
Morgikc <144792250+Morgikc@users.noreply.github.com>
2024-04-27 17:35:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "mainwindow.h"
 #include "task.h"
 #include <QApplication>
+#include "entertask.h"
+#include "secondwindow.h"
+#include "alltasks.h"
+#include "qdynamicbutton.h"
+#include "task.h"
 
 
 
